106835d1dff66d4fe03cbb9edb2f44c920ae0681
ddi <ddili6599@gmail.com>
2018-11-06 10:44:22 +0800

    ...

--- src/pages/Personal/index.js
+++ src/pages/Personal/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect, Link, withRouter } from 'react-router-dom'
 import connect from 'connect'
-import * as asyncFuns from "../../reducers/asyncExample";
 @connect
 @withRouter
 class Personal extends Component {
